fdd35fd0d306856f70f66d55315333bff30ed992
Sonja Broda <sonja@imap.cc>
2015-01-15 14:51:08 +0100

    remove ajaxify plugin and change script

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -6,25 +6,8 @@
   </footer>
  <script src="https://code.jquery.com/jquery.js"></script>
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
-<?php echo js('assets/js/jquery.ajaxify.min.js') ?>
 <script>
-$.Ajaxify.pageChange(function() {
    
-
-if ( $( '.panel-collapse' ).not(this).hasClass( "collapse" ) ) {
-        $('.panel-heading a h3').show();
-    }
-$(".panel-heading a").click(function() {
-    $('.panel-heading a').not(this).find('h3').show();
-    $(this).find('h3').toggle();
-    $(this).find('span').toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
-    $('html,body').animate({
-        scrollTop: $(this).offset().top
-    }, 2000);
-});
-});
-</script>
-<script>
 if ( $( '.panel-collapse' ).not(this).hasClass( "collapse" ) ) {
         $('.panel-heading a h3').show();
     }
@@ -37,10 +20,20 @@ $(".panel-heading a").click(function() {
     }, 2000);
 });
 
-</script>
+if ( $( '.panel-collapse' ).not(this).hasClass( "collapse" ) ) {
+        $('.panel-heading a h3').show();
+    }
+$(".panel-heading a").click(function() {
+    $('.panel-heading a').not(this).find('h3').show();
+    $(this).find('h3').toggle();
+    $(this).find('span').toggleClass('glyphicon-chevron-down glyphicon-chevron-up');
+    $('html,body').animate({
+        scrollTop: $(this).offset().top
+    }, 2000);
+});
 
-<script>
-$.Ajaxify.pageChange(function() {
+
+
 $('.open').click(function(){
     var href = $(this).attr('href');
     $('html, body').animate({
@@ -50,11 +43,16 @@ $('.open').click(function(){
     $('div ' +href).prev().find('h3').hide();
     
 });
-});
-</script>
-<script>
-    $.Ajaxify.container = '.shell';
-    $.Ajaxify.all();
+
+    var a=document.getElementsByTagName("a");
+for(var i=0;i<a.length;i++)
+{
+    a[i].onclick=function()
+    {
+        window.location=this.getAttribute("href");
+        return false
+    }
+}
 </script>
 
 
